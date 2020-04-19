c0ac7cfd9bca4835b85b2e0100f13cd964e35e00
kieu <kieu@gmail.com>
2020-04-19 23:34:58 +0700

    them comment

--- src/bai1hoancauthaychua/Bai1hoancauthaychua.java
+++ src/bai1hoancauthaychua/Bai1hoancauthaychua.java
@@ -17,7 +17,7 @@ public class Bai1hoancauthaychua {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic heree
         int luongTrungBinh = 0, tongLuong = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập số nhân viên trong công ty: ");
